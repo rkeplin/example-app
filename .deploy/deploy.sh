441657a540c069054026e89211f57438baa836a9
Rob <rkeplin@gmail.com>
2019-11-04 12:28:14 -0500

    Updated deployment process

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -3,9 +3,11 @@ openssl aes-256-cbc -K $encrypted_867d5633ae9d_key -iv $encrypted_867d5633ae9d_i
 
 chmod 0400 $(pwd)/.deploy/id_rsa
 
-rsync -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $(pwd)/.deploy/id_rsa" -avz --exclude '.deploy' --exclude '.git' $(pwd)/ travis@157.245.123.7:/opt/example-app/$TRAVIS_BUILD_NUMBER
-ssh -t -oStrictHostKeyChecking=no -i $(pwd)/.deploy/id_rsa travis@157.245.123.7 "chown -R apache:apache /opt/example-app/$TRAVIS_BUILD_NUMBER"
-ssh -t -oStrictHostKeyChecking=no -i $(pwd)/.deploy/id_rsa travis@157.245.123.7 "ln -sfn /opt/example-app/$TRAVIS_BUILD_NUMBER /var/www/html"
-ssh -t -oStrictHostKeyChecking=no -i $(pwd)/.deploy/id_rsa travis@157.245.123.7 "chown -h apache:apache /var/www/html"
+SERVER_IP="157.245.123.7"
+
+rsync -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $(pwd)/.deploy/id_rsa" -avz --exclude '.deploy' --exclude '.git' $(pwd)/ travis@$SERVER_IP:/opt/example-app/$TRAVIS_BUILD_NUMBER
+ssh -t -oStrictHostKeyChecking=no -i $(pwd)/.deploy/id_rsa travis@$SERVER_IP "chown -R travis:apache /opt/example-app/$TRAVIS_BUILD_NUMBER"
+ssh -t -oStrictHostKeyChecking=no -i $(pwd)/.deploy/id_rsa travis@$SERVER_IP "ln -sfn /opt/example-app/$TRAVIS_BUILD_NUMBER /var/www/html/app"
+ssh -t -oStrictHostKeyChecking=no -i $(pwd)/.deploy/id_rsa travis@$SERVER_IP "chown -h travis:apache /var/www/html/app"
 
 rm -rf $(pwd)/.deploy/id_rsa
